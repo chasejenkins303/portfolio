6484aff37504b676397a2b967a169d76f4397461
chasejenkins303 <chasejenkins303@gmail.com>
2024-08-21 20:43:41 -0400

    format

--- portfolio-frontend/src/pages/Home.tsx
+++ portfolio-frontend/src/pages/Home.tsx
@@ -4,9 +4,11 @@ function Home(){
         <div className="font-sans bg-mauve min-h-[100vh] w-full">
             <div className="min-h-[55vh] flex justify-center items-end mb-12">
                 <div className="flex flex-col justify-center items-center pt-12 gap-12">
-                    <h1 className="text-light text-bold text-8xl p-8 rounded-xl relative">Chase Jenkins <span className="absolute top-0 left-0 w-8 h-8 border-t-8 border-l-8 border-light"></span>
-            {/* Bottom-Right Corner Border */}
-            <span className="absolute bottom-0 right-0 w-8 h-8 border-b-8 border-r-8 border-light"></span></h1>
+                    <h1 className="text-light text-bold text-8xl p-8 rounded-xl relative">
+                        Chase Jenkins 
+                        <span className="absolute top-0 left-0 w-8 h-8 border-t-8 border-l-8 border-light"></span>
+                        <span className="absolute bottom-0 right-0 w-8 h-8 border-b-8 border-r-8 border-light"></span>
+                    </h1>
                     <div className="max-w-[35vw] text-mauve bg-light text-bold rounded-xl flex-col p-4 gap-2">
                         <p className="text-xl underline pb-4">Software Engineer</p>
                         <p className="text-lg">Hello, I am a software engineer with a passion for creating innovative, reliable, and useful software. Feel free to reach out via any of the listed methods and thank you for stopping by!</p>
