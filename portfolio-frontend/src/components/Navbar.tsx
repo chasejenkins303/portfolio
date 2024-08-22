9692ee82b1aa7b08a80f0ef7f5de36cd83ba6875
chasejenkins303 <chasejenkins303@gmail.com>
2024-08-21 23:50:30 -0400

    new button layout

--- portfolio-frontend/src/components/Navbar.tsx
+++ portfolio-frontend/src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 
 export default function Navbar() {
     return(
-        <div className="w-full h-16 bg-light flex justify-between items-center px-20 text-black">
+        <div className="w-full h-16 bg-black flex justify-between items-center px-20 text-light">
             <p className="text-xl">Chase Jenkins</p>
-            <div className="flex gap-16 text-lg">
-                <p >
+            <div className="flex items-center gap-16 text-lg h-full">
+                <p className="hover:cursor-pointer hover:underline">
                     Contact
                 </p>
-                <p>
+                <p className="hover:cursor-pointer hover:underline">
                     Experience
                 </p>
-                <p>
+                <p className="hover:cursor-pointer hover:underline">
                     Projects
                 </p>
             </div>
--- portfolio-frontend/src/pages/Home.tsx
+++ portfolio-frontend/src/pages/Home.tsx
@@ -3,13 +3,13 @@ function Home(){
     return (
         <div className="font-sans bg-mauve min-h-[100vh] w-full">
             <div className="min-h-[55vh] flex justify-center items-end mb-12">
-                <div className="flex flex-col justify-center items-center pt-12 gap-12">
+                <div className="flex flex-col justify-center items-center text-center pt-12 gap-12">
                     <h1 className="text-light text-bold text-8xl p-8 rounded-xl relative">
                         Chase Jenkins 
                         <span className="absolute top-0 left-0 w-8 h-8 border-t-8 border-l-8 border-light"></span>
                         <span className="absolute bottom-0 right-0 w-8 h-8 border-b-8 border-r-8 border-light"></span>
                     </h1>
-                    <div className="max-w-[35vw] text-mauve bg-light text-bold rounded-xl flex-col p-4 gap-2">
+                    <div className="max-w-[35vw] text-black bg-light text-bold rounded-xl flex-col p-4 gap-2">
                         <p className="text-xl underline pb-4">Software Engineer</p>
                         <p className="text-lg">Hello, I am a software engineer with a passion for creating innovative, reliable, and useful software. Feel free to reach out via any of the listed methods and thank you for stopping by!</p>
                     </div>
@@ -17,10 +17,14 @@ function Home(){
             </div>
 
 
-            <div className="flex px-24 justify-evenly pt-12 text-light text-lg">
-                <p className="underline hover:bg-light hover:text-mauve hover:rounded-lg hover:cursor-pointer p-4">Contact</p>
-                <p className="underline hover:bg-light hover:text-mauve hover:rounded-lg hover:cursor-pointer p-4">Experience</p>
-                <p className="underline hover:bg-light hover:text-mauve hover:rounded-lg hover:cursor-pointer p-4">Projects</p>
+            <div className="flex justify-center">
+                <div className="flex w-[75vw] px-36 justify-evenly items-center pt-12 text-light text-lg">
+                    <button className="px-4 py-2 bg-light text-black rounded hover:underline">Contact</button>
+                    <span className="mx-2 border-t-2 border-light flex-grow "></span>
+                    <button className="px-4 py-2 bg-light text-black rounded hover:underline">Experience</button>
+                    <span className="mx-2 border-t-2 border-light flex-grow"></span>
+                    <button className="px-4 py-2 bg-light text-black rounded hover:underline">Projects</button>
+                </div>
             </div>
         </div>
     )
